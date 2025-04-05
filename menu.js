1464d0f88d6a04f2b76af11f6d4685d14234d9af
Manukkz <jmbritovieira@gmail.com>
2025-04-04 21:42:40 -0300

    overlay fechando.

--- menu.js
+++ menu.js
@@ -1,10 +1,13 @@
 let jmMenu = document.getElementById("jm-social")
 let menu = document.getElementById("menu-mobile")
-
+let overlay = document.getElementById("overlay-menu")
 
 jmMenu.addEventListener("click",()=>{
     menu.classList.add("abrir-menu")
 })
 menu.addEventListener("click",()=>{
+    menu.classList.remove("abrir-menu")
+})
+overlay.addEventListener("click",()=>{
     menu.classList.remove("abrir-menu")
 })
